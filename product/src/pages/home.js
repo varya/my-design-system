e7f39f9689d347e3156ac3fb5ad0da3365c4ac5a
Bridge Tools <robot@bridgetools.io>
2021-05-06 13:37:31 +0300

    Changes to the product

--- product/src/pages/home.js
+++ product/src/pages/home.js
@@ -4,7 +4,7 @@
  */
 import React from "react";
 import styled from "styled-components";
-import { Button, Icon, colors } from "design-system";
+import { Avatar, Button, Icon, colors } from "design-system";
 import { Link } from "react-router-dom";
 
 const data = require("../data/data.js");
@@ -14,7 +14,7 @@ const Home = () => {
     <div>
       <StyledDiv1>
         <StyledDiv2>
-          <StyledImage1 src={data.user.avatar} />
+          <Avatar src={data.user.avatar} />
           <Button isOutline>
             <Icon name="menu" />
           </Button>
